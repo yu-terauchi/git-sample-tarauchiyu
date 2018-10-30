a935e02ed771adde483b67d52a25f235b40643c6
terauchi <yu.terauchi@rakus.co.jp>
2018-10-30 11:27:28 +0900

    新規クラスのCarクラスを追加

--- src/main/java/practice/Application.java
+++ src/main/java/practice/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("デプロイとサーバ起動を行います");
 		SpringApplication.run(Application.class, args);
 	}
 }
