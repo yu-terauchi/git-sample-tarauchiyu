6f95e0704a2b46aff9a253b1140bd80af4bd42a6
terauchi <yu.terauchi@rakus.co.jp>
2018-10-30 12:03:14 +0900

    first commit

--- src/main/java/practice/Car.java
+++ src/main/java/practice/Car.java
@@ -1,5 +1,37 @@
 package practice;
 
 public class Car {
-
+	private int speed;
+	private String bodyColor;
+	
+	public Car() {
+		speed = 0;
+		bodyColor = null;
+	}
+	
+	public Car(int speed, String bodyColor) {
+		super();
+		this.speed = speed;
+		this.bodyColor = bodyColor;
+	}
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+	}
+	public int getSpeed() {
+		speed += 30;
+		System.out.println("スピードが上がったよ");
+		System.out.println("ただ今のスピードは" + speed + "です");
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 }
+	
---
+++ src/main/java/practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		car.getSpeed();
+		car.getSpeed();
+		car.getSpeed();
+		
+	}
+
+}
